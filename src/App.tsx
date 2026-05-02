5e830e6394b8c2fc5c6525d076adeb89e538c066
Matheus Wilberstaedt <azevedowilber@icloud.com>
2026-05-02 22:26:50 +1000

    fix(routes): wire /agenda-cheia and /mwflow routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CleaningSystem from "./pages/CleaningSystem";
+import AgendaCheia from "./pages/AgendaCheia";
+import Mwflow from "./pages/Mwflow";
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/cleaning-system" element={<CleaningSystem />} />
       <Route path="/system" element={<CleaningSystem />} />
+      <Route path="/agenda-cheia" element={<AgendaCheia />} />
+      <Route path="/mwflow" element={<Mwflow />} />
     </Routes>
   );
 }
